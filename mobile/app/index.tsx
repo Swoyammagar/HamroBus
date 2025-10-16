2d90c665d806b8234400a5c78494622db4735c52
Swoyam <swoyammagar2016@gmail.com>
2025-10-16 22:03:32 +0545

    Added directory for developing passenger and driver mobile app

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="text-2xl justify-center flex-1 items-center"
+    >
+      <Text>welcome</Text>
+    </View>
+  );
+}
