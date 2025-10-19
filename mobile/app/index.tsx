d1bf6638dedb0a44b809532973c6190b8177139e
Swoyam <swoyammagar2016@gmail.com>
2025-10-19 22:13:22 +0545

    added a demo singup info page for drivers. needs refinement

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -2,10 +2,11 @@ import { Text, View } from "react-native";
 import Login from "./pages/mobilelogin";
 import VerifyOTP from "./pages/otpPassword";
 import Preference from "./pages/preference";
+import PersonalInfo from "./driver/signupInfo";
 export default function Index() {
   return (
     <View>
-      <Preference />
+      <PersonalInfo />
     </View>
   );
 }
