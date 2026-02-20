0d05f58987e02d4bd84e800381065b8b8f58d785
Swoyam <swoyammagar2016@gmail.com>
2026-02-20 21:06:57 +0545

    updated root layout.tsx to include the route context inthe children as a layer.

--- admin/app/_layout.tsx
+++ admin/app/_layout.tsx
@@ -3,13 +3,20 @@ import { Stack } from "expo-router";
 import "./global.css";
 import { AuthProvider } from "./context/AuthContext";
 import { DriverProvider } from "./context/DriverContext";
+import { RouteProvider } from "./context/RouteContext";
+import { BusProvider } from "./context/BusContext";
 
-// Wrap the entire app with providers so useAuth and useDriver can be used in pages
+
+// Wrap the entire app with providers so useAuth, useDriver, useRoute, and useBus can be used in pages
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
       <DriverProvider>
-        <Stack>{children}</Stack>
+        <RouteProvider>
+          <BusProvider>
+            <Stack>{children}</Stack>
+          </BusProvider>
+        </RouteProvider>
       </DriverProvider>
     </AuthProvider>
   );
