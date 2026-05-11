28879fc88e49170217e2f17cb1542acba04a5e1c
Swoyam <swoyammagar2016@gmail.com>
2026-05-11 19:33:58 +0545

    Added cookie token in the chat auth middleware.

--- Backend/middlewares/chatAuth.middleware.js
+++ Backend/middlewares/chatAuth.middleware.js
@@ -4,7 +4,9 @@ const Admin = require('../models/admin.model');
 
 const chatAuth = async (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(' ')[1];
+    const authHeaderToken = req.headers.authorization?.split(' ')[1];
+    const cookieToken = req.cookies?.access_token;
+    const token = authHeaderToken || cookieToken;
 
     if (!token) {
       return res.status(401).json({ message: 'No token provided' });
